caba3d8db6a35051c26b20ae200d5a7eb8063296
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-14 10:33:00 +0000

    Adding further progress to have_turn function and tests.

--- src/have_turn.py
+++ src/have_turn.py
@@ -1,2 +1,8 @@
 def have_turn(turn, number, given_board):
-    return []
+    if number > 0 and number < 4:
+        given_board[0][number - 1] = turn
+    elif number > 3 and number < 7:
+        given_board[1][number - 4] = turn
+    elif number > 6 and number < 10:
+        given_board[2][number - 7] = turn
+    return given_board
--- test/test_have_turn.py
+++ test/test_have_turn.py
@@ -7,5 +7,18 @@ class TestHaveTurn:
         output = have_turn("O", 1, [[[], [], []], [[], [], []], [[], [], []]])
         assert isinstance(output, list)
 
-    def test_function_adds_naught_to_function(self):
-        pass
+    def test_function_adds_naught_to_board_at_correct_position(self):
+        input_turn = "X"
+        input_number = 3
+        input_board = [[[], [], []], [[], [], []], [[], [], []]]
+        expected = [[[], [], "X"], [[], [], []], [[], [], []]]
+        output = have_turn(input_turn, input_number, input_board)
+        assert output == expected
+
+    def test_function_adds_cross_to_board_at_correct_position(self):
+        input_turn = "O"
+        input_number = 8
+        input_board = [[[], [], []], [[], [], []], [[], [], []]]
+        expected = [[[], [], []], [[], [], []], [[], "O", []]]
+        output = have_turn(input_turn, input_number, input_board)
+        assert output == expected
