76992e1d81e99f457f5b11c10eb263953446ae5e
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-17 18:20:21 +0000

    Adding board to naughts_and_crosses function.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
+from src.get_board import get_board
+from src.check_board_format import check_board_format
+
+
 def naughts_and_crosses():
-    pass
+    board = get_board()
+    check_board_format(board)
