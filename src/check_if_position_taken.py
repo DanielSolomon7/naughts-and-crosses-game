b6147fe4de2bc12235b5b566cf3b0989d3a93ec2
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-17 14:44:46 +0000

    Adding check_if_position_taken function with tests.

--- src/check_if_position_taken.py
+++ src/check_if_position_taken.py
@@ -1,2 +1,22 @@
-def check_if_position_taken():
-    pass
+def check_if_position_taken(position_num, given_board):
+    if not isinstance(position_num, int):
+        raise TypeError("TypeError: given postion number must be an integer from 1-9.")
+    if position_num < 1 or position_num > 9:
+        raise ValueError(
+            "ValueError: given postion number must be an integer from 1-9."
+        )
+
+    if position_num > 0 and position_num < 4:
+        row_number = 0
+        column_num = position_num - 1
+    elif position_num > 3 and position_num < 7:
+        row_number = 1
+        column_num = position_num - 4
+    elif position_num > 6 and position_num < 10:
+        row_number = 2
+        column_num = position_num - 7
+
+    if given_board[row_number][column_num] == []:
+        return False
+    else:
+        return True
--- test/test_check_if_position_taken.py
+++ test/test_check_if_position_taken.py
@@ -1,5 +1,67 @@
 from src.check_if_position_taken import check_if_position_taken
+import pytest
+
 
 class TestCheckIfPositionTaken:
-    def test_(self):
-        pass
+    def test_function_returns_a_boolean(self):
+        input_num = 1
+        input_board = [[[], [], []], [[], [], []], [[], []]]
+        output = check_if_position_taken(input_num, input_board)
+        assert isinstance(output, bool)
+
+    def test_function_identifies_empty_board(self):
+        input_num = 1
+        input_board = [[[], [], []], [[], [], []], [[], []]]
+        output = check_if_position_taken(input_num, input_board)
+        assert output == False
+
+    def test_function_identifies_position_not_taken(self):
+        input_num = 1
+        input_board = [[[], "X", []], [[], [], []], [[], []]]
+        output = check_if_position_taken(input_num, input_board)
+        assert output == False
+
+    def test_function_identifies_position_taken(self):
+        input_num = 1
+        input_board = [["O", [], []], [[], [], []], [[], []]]
+        output = check_if_position_taken(input_num, input_board)
+        assert output == True
+
+        input_num = 5
+        input_board = [[[], [], []], [[], "X", []], [[], []]]
+        output = check_if_position_taken(input_num, input_board)
+        assert output == True
+
+        input_num = 9
+        input_board = [[[], [], []], [[], [], []], [[], [], "O"]]
+        output = check_if_position_taken(input_num, input_board)
+        assert output == True
+
+    def test_function_handles_invalid_position_number_type(self):
+        input_num = "Hi"
+        input_board = [["O", [], []], [[], [], []], [[], []]]
+        with pytest.raises(TypeError) as e:
+            check_if_position_taken(input_num, input_board)
+        assert (
+            str(e.value)
+            == "TypeError: given postion number must be an integer from 1-9."
+        )
+
+    def test_function_handles_out_of_range_position_number(self):
+        input_num = 0
+        input_board = [["O", [], []], [[], [], []], [[], []]]
+        with pytest.raises(ValueError) as e:
+            check_if_position_taken(input_num, input_board)
+        assert (
+            str(e.value)
+            == "ValueError: given postion number must be an integer from 1-9."
+        )
+
+        input_num = 10
+        input_board = [["O", [], []], [[], [], []], [[], []]]
+        with pytest.raises(ValueError) as e:
+            check_if_position_taken(input_num, input_board)
+        assert (
+            str(e.value)
+            == "ValueError: given postion number must be an integer from 1-9."
+        )
