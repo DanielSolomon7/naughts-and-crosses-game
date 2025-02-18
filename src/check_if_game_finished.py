2d913c3af043ac789db40d20571ae974bee0a186
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-18 10:37:54 +0000

    Adding setup for check_if_game_finished function.

---
+++ src/check_if_game_finished.py
@@ -0,0 +1,2 @@
+def check_if_game_finished(given_board):
+    pass
---
+++ test/test_check_if_game_finished.py
@@ -0,0 +1,6 @@
+from src.check_if_game_finished import check_if_game_finished
+
+
+class TestCheckIfGameFinished:
+    def test_(self):
+        pass
