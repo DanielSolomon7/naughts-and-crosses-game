bbc4df1588e1b2ec11a723f176a2befd1324db0f
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-19 11:52:12 +0000

    Adding working check_if_game_finished_function.

--- src/check_if_game_finished.py
+++ src/check_if_game_finished.py
@@ -1,2 +1,63 @@
 def check_if_game_finished(given_board):
-    pass
+    cross = "X"
+    naught = "O"
+
+    """Check rows"""
+    for i in range(3):
+        if (
+            given_board[i][0] == cross
+            and given_board[i][1] == cross
+            and given_board[i][2] == cross
+        ):
+            return True
+        elif (
+            given_board[i][0] == naught
+            and given_board[i][1] == naught
+            and given_board[i][2] == naught
+        ):
+            return True
+
+    """Check columns"""
+    for i in range(3):
+        if (
+            given_board[0][i] == cross
+            and given_board[1][i] == cross
+            and given_board[2][i] == cross
+        ):
+            return True
+        elif (
+            given_board[0][i] == naught
+            and given_board[1][i] == naught
+            and given_board[2][i] == naught
+        ):
+            return True
+
+    """Check diagonal left to right"""
+    if (
+        given_board[0][0] == cross
+        and given_board[1][1] == cross
+        and given_board[2][2] == cross
+    ):
+        return True
+    elif (
+        given_board[0][0] == naught
+        and given_board[1][1] == naught
+        and given_board[2][2] == naught
+    ):
+        return True
+
+    """Check diagonal right to left"""
+    if (
+        given_board[0][2] == cross
+        and given_board[1][1] == cross
+        and given_board[2][0] == cross
+    ):
+        return True
+    elif (
+        given_board[0][2] == naught
+        and given_board[1][1] == naught
+        and given_board[2][0] == naught
+    ):
+        return True
+
+    return False
--- test/test_check_if_game_finished.py
+++ test/test_check_if_game_finished.py
@@ -2,5 +2,66 @@ from src.check_if_game_finished import check_if_game_finished
 
 
 class TestCheckIfGameFinished:
-    def test_(self):
-        pass
+    def test_function_returns_a_boolean(self):
+        input = [[[], [], []], [[], [], []], [[], [], []]]
+        output = check_if_game_finished(input)
+        assert isinstance(output, bool)
+
+    def test_function_indentifies_empty_board(self):
+        input = [[[], [], []], [[], [], []], [[], [], []]]
+        output = check_if_game_finished(input)
+        assert output == False
+
+    def test_function_indentifies_unfinished_empty_game(self):
+        input = [[[], [], []], [[], [], []], [[], [], []]]
+        output = check_if_game_finished(input)
+        assert output == False
+
+    def test_function_indentifies_unfinished_but_not_empty_game(self):
+        input = [["O", [], "X"], ["X", "O", []], [[], "O", "X"]]
+        output = check_if_game_finished(input)
+        assert output == False
+
+        input = [["X", "X", []], [[], "O", "O"], ["X", [], "O"]]
+        output = check_if_game_finished(input)
+        assert output == False
+
+    def test_function_indentifies_finished_game_from_top_row(self):
+        input = [["X", "X", "X"], ["O", "O", []], [[], [], []]]
+        output = check_if_game_finished(input)
+        assert output == True
+
+    def test_function_indentifies_finished_game_from_middle_row(self):
+        input = [["X", [], "X"], ["O", "O", "O"], [[], [], []]]
+        output = check_if_game_finished(input)
+        assert output == True
+
+    def test_function_indentifies_finished_game_from_bottom_row(self):
+        input = [[[], [], []], ["O", [], "O"], ["X", "X", "X"]]
+        output = check_if_game_finished(input)
+        assert output == True
+
+    def test_function_indentifies_finished_game_from_left_column(self):
+        input = [["O", [], []], ["O", [], "X"], ["O", "X", "X"]]
+        output = check_if_game_finished(input)
+        assert output == True
+
+    def test_function_indentifies_finished_game_from_middle_column(self):
+        input = [["O", "X", []], [[], "X", "X"], ["O", "X", "O"]]
+        output = check_if_game_finished(input)
+        assert output == True
+
+    def test_function_indentifies_finished_game_from_right_column(self):
+        input = [["O", "X", "O"], [[], [], "O"], ["X", "X", "O"]]
+        output = check_if_game_finished(input)
+        assert output == True
+
+    def test_function_indentifies_finished_game_from_diagonal_left_to_right(self):
+        input = [["O", "X", "O"], [[], "O", []], ["X", "X", "O"]]
+        output = check_if_game_finished(input)
+        assert output == True
+
+    def test_function_indentifies_finished_game_from_diagonal_right_to_left(self):
+        input = [["O", [], "X"], [[], "X", []], ["X", "O", "O"]]
+        output = check_if_game_finished(input)
+        assert output == True
