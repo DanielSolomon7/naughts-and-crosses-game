2ac1199d27888039e1dbd923ce8dee14f7b202b1
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-20 16:28:09 +0000

    Adding further work on check_if_naught_or_cross function.

---
+++ src/check_if_naught_or_cross.py
@@ -0,0 +1,4 @@
+def check_if_naught_or_cross(input):
+    if input == "O" or input == "X":
+        return True
+    return False
--- test/test_check_if_naught_or_cross.py
+++ test/test_check_if_naught_or_cross.py
@@ -2,5 +2,26 @@ from src.check_if_naught_or_cross import check_if_naught_or_cross
 
 
 class TestCheckIfNaughtOrCross:
-    def test_(self):
-        pass
+    def test_function_returns_a_boolean(self):
+        input = "X"
+        output = check_if_naught_or_cross(input)
+        assert isinstance(output, bool)
+
+    def test_function_identifies_a_cross(self):
+        input = "X"
+        output = check_if_naught_or_cross(input)
+        assert output == True
+
+    def test_function_identifies_a_naught(self):
+        input = "O"
+        output = check_if_naught_or_cross(input)
+        assert output == True
+
+    def test_function_identifies_if_input_is_not_a_naught_or_cross(self):
+        input = "5"
+        output = check_if_naught_or_cross(input)
+        assert output == False
+
+        input = 10
+        output = check_if_naught_or_cross(input)
+        assert output == False
