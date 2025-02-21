828e09399020cd0833561784b7f30d026af94910
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-21 11:12:59 +0000

    Reformatting naughts_and_crosses function in main.py to use select_player function.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from src.get_board import get_board
 from src.check_board_format import check_board_format
 from src.check_if_naught_or_cross import check_if_naught_or_cross
+from src.select_player import select_player
 
 
 def naughts_and_crosses():
@@ -12,11 +13,9 @@ def naughts_and_crosses():
             if not check_if_naught_or_cross(player1):
                 print("Incorrect input. Please enter either 'O' or 'X'")
             else:
-                if player1 == "O":
-                    player2 = "X"
-                else:
-                    player2 = "O"
+                player2 = select_player(player1)["Player 2"]
                 players_selected = True
                 print(f"Player 1 is {player1}, and Player 2 is {player2}.")
+
 
 naughts_and_crosses()
