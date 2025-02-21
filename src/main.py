0eff6fdfc709d038d6fc1e9a7282adfce1c3dd8a
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-21 10:58:14 +0000

    Adding further progress to naughts_and_crosses function in main.py. If 'O' or 'X' is not selected, then the input will appear again. If the selection is valid, then a print statement shows which player is 'O', and which player is 'X'.

--- src/main.py
+++ src/main.py
@@ -6,11 +6,17 @@ from src.check_if_naught_or_cross import check_if_naught_or_cross
 def naughts_and_crosses():
     board = get_board()
     if check_board_format(board) == True:
-        player1 = input("Player 1 - Please choose 'O' or 'X':")
-        if not check_if_naught_or_cross(player1):
-            print("Incorrect input. Please enter either 'O' or 'X'")
-        else:
-            if player1 == "O":
-                player2 = "X"
+        players_selected = False
+        while players_selected == False:
+            player1 = input("Player 1 - Please choose 'O' or 'X':")
+            if not check_if_naught_or_cross(player1):
+                print("Incorrect input. Please enter either 'O' or 'X'")
             else:
-                player2 == "O"
+                if player1 == "O":
+                    player2 = "X"
+                else:
+                    player2 = "O"
+                players_selected = True
+                print(f"Player 1 is {player1}, and Player 2 is {player2}.")
+
+naughts_and_crosses()
