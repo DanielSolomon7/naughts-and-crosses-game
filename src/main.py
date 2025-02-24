162c4148a145497e3edf43315eb4820b42c9a2da
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-24 18:37:35 +0000

    Adding more code to naughts_and_crosses function. Adding a game_finished variable and while loop.

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def naughts_and_crosses():
     board = get_board()
     if check_board_format(board) == True:
         players_selected = False
-        while players_selected == False:
+        while not players_selected:
             player1 = input("Player 1 - Please choose 'O' or 'X':")
             if not check_if_naught_or_cross(player1):
                 print("Incorrect input. Please enter either 'O' or 'X'")
@@ -16,6 +16,10 @@ def naughts_and_crosses():
                 player2 = select_player2(player1)
                 players_selected = True
                 print(f"Player 1 is {player1}, and Player 2 is {player2}.")
+
+        game_finished = False
+        while not game_finished:
+            game_finished = True
 
 
 naughts_and_crosses()
