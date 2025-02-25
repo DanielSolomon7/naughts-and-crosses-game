3154503d7827cc9e072d79e842876f349865fb53
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-25 10:22:33 +0000

    Adding selection_board function, with tests.

--- src/selection_board.py
+++ src/selection_board.py
@@ -1,2 +1,18 @@
 def selection_board(given_board):
-    pass
+    counter = 0
+    for row in given_board:
+        index = 0
+        for cell in row:
+            counter += 1
+            if cell == []:
+                row[index] = str(counter)
+            index += 1
+
+    board = f"""   
+ {given_board[0][0]} | {given_board[0][1]} | {given_board[0][2]} 
+-----------
+ {given_board[1][0]} | {given_board[1][1]} | {given_board[1][2]} 
+-----------
+ {given_board[2][0]} | {given_board[2][1]} | {given_board[2][2]} 
+"""
+    return board
--- test/test_selection_board.py
+++ test/test_selection_board.py
@@ -2,5 +2,45 @@ from src.selection_board import selection_board
 
 
 class TestSelectionBoard:
-    def test_(self):
-        pass
+    def test_function_returns_a_string(self):
+        input = [[[], [], []], [[], [], []], [[], [], []]]
+        output = selection_board(input)
+        assert isinstance(output, str)
+
+    def test_function_identifies_empty_board_and_returns_str_in_selection_format(self):
+        input = [[[], [], []], [[], [], []], [[], [], []]]
+        expected = """   
+ 1 | 2 | 3 
+-----------
+ 4 | 5 | 6 
+-----------
+ 7 | 8 | 9 
+"""
+        output = selection_board(input)
+        assert output == expected
+
+    def test_function_identifies_filled_board(self):
+        input = [["X", "O", "X"], ["O", "O", "X"], ["X", "X", "O"]]
+        expected = """   
+ X | O | X 
+-----------
+ O | O | X 
+-----------
+ X | X | O 
+"""
+        output = selection_board(input)
+        assert output == expected
+
+    def test_function_identifies_partly_filled_board_and_returns_str_in_selection_format(
+        self,
+    ):
+        input = [["X", [], "X"], [[], "O", []], ["X", "O", []]]
+        expected = """   
+ X | 2 | X 
+-----------
+ 4 | O | 6 
+-----------
+ X | O | 9 
+"""
+        output = selection_board(input)
+        assert output == expected
