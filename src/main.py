eb130c0ca3b5e9719f21a9e352e9e95a40f266bf
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-25 10:56:21 +0000

    Adding player turn functionality to naughts_and_crosses function, in main.py.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from src.get_board import get_board
 from src.check_board_format import check_board_format
 from src.check_if_naught_or_cross import check_if_naught_or_cross
 from src.select_player2 import select_player2
+from src.board_to_print import board_to_print
+from src.selection_board import selection_board
+from src.have_turn import have_turn
 
 
 def naughts_and_crosses():
@@ -20,7 +23,20 @@ def naughts_and_crosses():
 
         # Start Game
         game_finished = False
+        player_turn = player2
         while not game_finished:
+            if player_turn == player1:
+                player_turn = player2
+            else:
+                player_turn = player1
+
+            print(board_to_print(board))
+            print(selection_board(board))
+            turn_number = int(input(f"Player {player_turn} - Select position on the board (1-9):"))
+            board = have_turn(player_turn, turn_number, board)
+            print(board_to_print(board))
+            print(f"Player {player_turn} selected postion {turn_number} on the board.")
+
             game_finished = True
 
 
