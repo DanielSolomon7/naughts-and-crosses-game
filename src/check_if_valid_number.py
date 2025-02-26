8553e1e3da82921ce3a372680212250f933832e7
Daniel Solomon <danielsolomon@mailfence.com>
2025-02-26 10:12:38 +0000

    Adding check_if_valid_number function setup.

---
+++ src/check_if_valid_number.py
@@ -0,0 +1,2 @@
+def check_if_valid_number(given_num):
+    pass
---
+++ test/test_check_if_valid_number.py
@@ -0,0 +1,5 @@
+from src.check_if_valid_number import check_if_valid_number
+
+class TestCheckIfValidNumber:
+    def test_(self):
+        pass
