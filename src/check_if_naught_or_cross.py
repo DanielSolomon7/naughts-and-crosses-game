f0f2723f1faa7c6f462998a2490dbcf7a1a32b66
Daniel Solomon <danielsolomon@mailfence.com>
2025-03-03 10:03:51 +0000

    Added docstring to check_if_naught_or_cross function.

--- src/check_if_naught_or_cross.py
+++ src/check_if_naught_or_cross.py
@@ -1,4 +1,13 @@
 def check_if_naught_or_cross(input):
+    """Checks if the given input is a valid naught or cross
+
+    Args:
+        input: a given input
+
+    Returns:
+        boolean: returns True if a valid naught or cross,
+                 returns False if not a valid naught or cross
+    """
     if input == "O" or input == "X":
         return True
     return False
