89a8defe6c325ef98ca5fd78041092d9af519d89
Daniel Solomon <danielsolomon@mailfence.com>
2025-03-03 10:01:44 +0000

    Added docstring to check_if_draw function.

--- src/check_if_draw.py
+++ src/check_if_draw.py
@@ -1,4 +1,14 @@
 def check_if_draw(given_board):
+    """Checks if a naughts and crosses game is finished
+    as a draw
+
+    Args:
+        given_board: a list of 3 lists (each list with a length of 3)
+
+    Returns:
+        boolean: returns True if finished as a draw,
+                 returns False if not finished as a draw
+    """
     finish = True
     for row in given_board:
         for cell in row:
