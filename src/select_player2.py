52013d98f4bedd64e7e98b3592d7cbd7a62b4d67
Daniel Solomon <danielsolomon@mailfence.com>
2025-03-04 12:52:12 +0000

    Adding docstring for select_player2 function.

--- src/select_player2.py
+++ src/select_player2.py
@@ -1,4 +1,13 @@
 def select_player2(player1_input):
+    """Identifies if player 1 is a naught or cross, and returns
+    what player 2 will be
+
+    Args:
+        player1_input: given input, which should be a string of "O" or "X"
+
+    Returns:
+        string: a string of player 2, whether "O" or "X"
+    """
     if player1_input == "O":
         return "X"
     elif player1_input == "X":
