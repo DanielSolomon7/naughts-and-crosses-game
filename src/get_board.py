3325baafae625f9ac621b11f2e14d11d38276ef5
Daniel Solomon <danielsolomon@mailfence.com>
2025-03-04 12:48:43 +0000

    Adding docstring for get_board function.

--- src/get_board.py
+++ src/get_board.py
@@ -1,2 +1,7 @@
 def get_board():
+    """Returns an empty naughts and crosses board
+
+    Returns:
+        list: a list of 3 lists (each list with 3 empty lists)
+    """
     return [[[], [], []], [[], [], []], [[], [], []]]
