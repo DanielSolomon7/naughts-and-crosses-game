68278de2a6901acd0f9220a1afef065b4fba3330
Daniel Solomon <danielsolomon@mailfence.com>
2025-03-05 10:41:31 +0000

    Adding docstring to check_if_valid_number function.

--- src/check_if_valid_number.py
+++ src/check_if_valid_number.py
@@ -1,4 +1,14 @@
 def check_if_valid_number(given_num):
+    """Checks if a given number is a valid board position on
+    a naughts and crosses board
+
+    Args:
+        given_num: a given input
+
+    Returns:
+        boolean: returns True if a valid number,
+                 returns False if not a valid number
+    """
     if given_num in [
         "1",
         "2",
