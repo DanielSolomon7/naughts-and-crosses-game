25c03554e15f6c668a41e67ed4be90d7354e8fad
Daniel Solomon <danielsolomon@mailfence.com>
2025-03-06 16:47:59 +0000

    Adding docstring to have_turn function.

--- src/have_turn.py
+++ src/have_turn.py
@@ -1,4 +1,15 @@
 def have_turn(turn, position_number, given_board):
+    """Place a naught or cross on a board
+
+    Args:
+        turn (str): either "O" or "X"
+        postion_number (int): a position number for the board (should be from 1-9)
+        given_board (list): a list of 3 lists (each list with a length of 3)
+
+    Returns:
+        list: an updated version of the given board, with the naught or
+              cross added to it
+    """
     if turn != "X" and turn != "O":
         raise ValueError("ValueError: given turn must be either 'X' or 'O'")
     if not isinstance(position_number, int):
